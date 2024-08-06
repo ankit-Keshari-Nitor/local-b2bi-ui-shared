d9c7873287d595e154b15790afb82eff7990beff
Pradeep K <Pradeep.K@precisely.com>
2024-08-06 12:39:50 +0530

    PEM-27639: Fix for Data table row actions clone issue

--- packages/shell/src/components/DataTable/DataTableUtil.js
+++ packages/shell/src/components/DataTable/DataTableUtil.js
@@ -3,6 +3,28 @@ class DataTableUtil {
     return Object.fromEntries(array.map((obj) => [obj[objectKey], obj]));
   }
 
+  static deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+      return obj;
+    }
+
+    if (Array.isArray(obj)) {
+      const arrCopy = [];
+      for (let i = 0; i < obj.length; i++) {
+        arrCopy[i] = DataTableUtil.deepClone(obj[i]);
+      }
+      return arrCopy;
+    }
+
+    const objCopy = {};
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        objCopy[key] = DataTableUtil.deepClone(obj[key]);
+      }
+    }
+    return objCopy;
+  }
+
   static getSelectedRowData(selectedRows, rowObj) {
     const selectedRowData = [];
     selectedRows.forEach((selectedRow) => {
@@ -12,23 +34,24 @@ class DataTableUtil {
   }
 
   static filterActions(selectedRows, actions, filterFn) {
-    actions.forEach((action) => {
+    const tempActions = DataTableUtil.deepClone(actions);
+    tempActions.forEach((action) => {
       action.shouldShow = true;
       action.isVisible = action.isVisible || action.isVisible === undefined;
     });
-    const actionsObj = Object.fromEntries(actions.map((obj) => [obj.id, obj]));
+    const actionsObj = Object.fromEntries(tempActions.map((obj) => [obj.id, obj]));
     selectedRows.forEach((selectedRow) => {
       if (filterFn !== undefined) {
         filterFn(selectedRow, actionsObj);
       }
     });
-    actions.forEach((action) => {
+    tempActions.forEach((action) => {
       if (action.selection === 'single' && selectedRows.length > 1) {
         action.shouldShow = false;
         action.isVisible = false;
       }
     });
-    return actions;
+    return tempActions;
   }
 
   static paginate(data, { page, pageSize }) {
